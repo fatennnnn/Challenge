fc2c860806720d1e6349c3057535a24120842757
yahyaoui-mohamed-bayrem <y.bayrem@gmail.com>
2020-02-20 14:01:39 +0100

    carosel

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MyCarroselle from './components/MyCarroselle';
 
 function App() {
   return (
     <div className="App">
-      
+      <MyCarroselle/>
     </div>
   );
 }
